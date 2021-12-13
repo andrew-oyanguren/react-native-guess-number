b655ad4c3f1ad674786ec3f1120cae554af2805d
andrew-oyanguren <aaoyanguren@outlook.com>
2021-12-12 20:42:22 -0800

    Added images to gameover screen.

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,12 +1,31 @@
 import React from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import { View, Text, StyleSheet, Button, Image } from "react-native";
+
+import DefaultStyles from "../constants/default-styles";
+import BodyText from "../components/BodyText";
+import colors from "../constants/colors";
+import { Colors } from "react-native/Libraries/NewAppScreen";
 
 const GameOverScreen = (props) => {
   return (
     <View style={styles.screen}>
-      <Text>The Game is Over!</Text>
-      <Text>Guesses: {props.roundsNumber}</Text>
-      <Text>Number was: {props.userNumber}</Text>
+      <Text style={DefaultStyles.title}>The Game is Over!</Text>
+      <View style={styles.imageContainer}>
+        <Image
+          source={{
+            uri: "https://cdn.pixabay.com/photo/2016/05/05/23/52/mountain-summit-1375015_1280.jpg",
+          }}
+          style={styles.image}
+        />
+      </View>
+      <View style={styles.resultsContainer}>
+        <BodyText style={styles.textAlignContainer}>
+          Your phone needed{" "}
+          <Text style={styles.highlight}>{props.roundsNumber}</Text> rounds to
+          guess the number{" "}
+          <Text style={styles.highlight}>{props.userNumber}</Text>.
+        </BodyText>
+      </View>
       <Button title="New Game" onPress={props.onStartOver} />
     </View>
   );
@@ -17,6 +36,31 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+  },
+  imageContainer: {
+    borderRadius: 150,
+    borderWidth: 3,
+    borderColor: "black",
+    width: 300,
+    height: 300,
+    overflow: "hidden",
+    marginVertical: 20,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+  resultsContainer: {
+    width: "75%",
+    marginVertical: 30,
+  },
+  highlight: {
+    color: Colors.primary,
+    fontFamily: "open-sans-bold",
+  },
+  textAlignContainer: {
+    textAlign: "center",
+    fontSize: 20,
   },
 });
 
